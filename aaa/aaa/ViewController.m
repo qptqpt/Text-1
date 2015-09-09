bce08db4c55abf624d0c34e7854a8cee46578e3d
lanou <798729330@qq.com>
2015-09-09 17:00:05 +0800

    first

---
+++ aaa/aaa/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  aaa
+//
+//  Created by lanou on 15/9/9.
+//  Copyright (c) 2015年 Ezio. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    // 刷卡缴费卡就是看到积分卡萨
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
